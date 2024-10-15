adfeebc5a737fdbf6e4eb413ca5cbf43d273b616
novais <mathpn2001@gmail.com>
2024-10-14 21:27:20 -0300

    changing main for num gen

--- primalidade/main.py
+++ primalidade/main.py
@@ -11,36 +11,45 @@ def generate_prime_number(lenght, generator, test_method) -> int:
 
     is_prime = False
 
-    start_time = 0
-    end_time = 0
 
     while not is_prime :
-        start_time = time()
-
         random_number = generator.lcg(lenght)
         # random_number = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
         if test_method(random_number, number_of_loops):
             is_prime = True
 
-        end_time = time()
-
-    print("Numero gerado: ", random_number)
-    print("Lenght: ", random_number.bit_length(), "bits.")
-    print("Tempo de execução: {:.8f} segundos.\n".format(end_time - start_time))
 
     return random_number
 
 if __name__ == "__main__":
-    bit_lengths =  [40, 56, 80, 128, 168, 224, 256, 512, 1024, 2048, 4096]
+    bit_lengths =  [40, 56, 80, 128, 168, 224, 256, 512, 1024]
+    bit_lengths_bigger = [2048, 4096]
 
-        # # Teste com o metodo de Miller-Rabin
-        # print("Miller-Rabin")
-        # for lenght in bit_lengths:
-        #     lcg = LinearCongruentialGenerator()
-        #     number_generated = generate_prime_number(lenght, lcg, is_prime_miller_rabin)
+    # print("Miller-Rabin")
+    # for lenght in bit_lengths_bigger:
+    #     times = []
+    #     for i in range(5):
+    #         start = time()
+    #         lcg = LinearCongruentialGenerator()
+    #         number_generated = generate_prime_number(lenght, lcg, is_prime_miller_rabin)
+    #         end = time()
+    #         times.append(end - start)
+    #         if i == 0:
+    #             print("Numero primo gerado: {}".format(number_generated))
+    #     print("Length: {} bits".format(lenght))
+    #     print("Tempo de execução: {:.8f} segundos.\n".format((sum(times) / len(times))))
 
     # Teste com o metodo de Fermat
     print("Fermat")
-    for lenght in bit_lengths:
-        lcg = LinearCongruentialGenerator()
-        number_generated = generate_prime_number(lenght, lcg, is_prime_fermat)
+    for lenght in bit_lengths_bigger:
+        times = []
+        for i in range(5):
+            start = time()
+            lcg = LinearCongruentialGenerator()
+            number_generated = generate_prime_number(lenght, lcg, is_prime_fermat)
+            end = time()
+            times.append(end - start)
+            if i == 0:
+                print("Numero primo gerado: {}".format(number_generated))
+        print("Length: {} bits".format(lenght))
+        print("Tempo de execução: {:.8f} segundos.\n".format((sum(times)/len(times))))
--- pseudo_aleatorio/main.py
+++ pseudo_aleatorio/main.py
@@ -8,24 +8,24 @@ from random import randint
 if __name__ == "__main__":
     bit_lenght = [40, 56, 80, 128, 168, 224, 256, 512, 1024, 2048, 4096]
 
+    # for lenght in bit_lenght:
+    #     times = []
+    #     for _ in range(1000):
+    #         lcg = LinearCongruentialGenerator()
+    #         start = time()
+    #         number_generated = lcg.lcg(lenght)
+    #         end = time()
+    #         times.append(end - start)
+    #     print("Length: {} bits".format(lenght))
+    #     print("Tempo de execução: {:.8f} segundos.\n".format((sum(times)/len(times))))
+    #
     for lenght in bit_lenght:
-        lcg = LinearCongruentialGenerator()
-        start = time()
-        number_generated = lcg.lcg(lenght)
-        end = time()
-        print("Linear Congruential Generator")
-        print("Numero gerado: {}".format(number_generated))
+        times = []
+        for _ in range(1000):
+            pm = ParkMiller()
+            start = time()
+            number_generated = pm.pm(lenght)
+            end = time()
+            times.append(end - start)
         print("Length: {} bits".format(lenght))
-        print("Tempo de execução: {:.8f} segundos.\n".format(end - start))
-
-    print("\n--------------------------------------------------------------------------------\n")
-
-    for lenght in bit_lenght:
-        pm = ParkMiller()
-        start = time()
-        number_generated = pm.pm(lenght)
-        end = time()
-        print("Park Miller Generator")
-        print("Numero gerado: {}".format(number_generated))
-        print("Length: {} bits".format(lenght))
-        print("Tempo de execução: {:.8f} segundos.\n".format(end - start))
+        print("Tempo de execução: {:.8f} segundos.\n".format((sum(times)/len(times))))
